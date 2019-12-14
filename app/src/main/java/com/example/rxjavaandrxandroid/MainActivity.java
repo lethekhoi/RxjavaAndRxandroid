7cd858ed8ceb81ed6d26829a4eb8dfa06f895100
lethekhoi <lethekhoi@gmail.com>
2019-12-14 20:30:19 +0700

    finish Rx

--- app/src/main/java/com/example/rxjavaandrxandroid/MainActivity.java
+++ app/src/main/java/com/example/rxjavaandrxandroid/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -11,16 +12,45 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    Disposable disposable;
+    CompositeDisposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Observable<String> observableNames = Observable.just("Teo", "Ti", "Tun");
+        observableNames.subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread()) // Rxjava
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable.add(d);
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
 //        List<String> arrayNames = new ArrayList<>();
 //        arrayNames.add("Nguyen Van A");
 //        arrayNames.add("Nguyen Van B");
@@ -30,27 +60,27 @@ public class MainActivity extends AppCompatActivity {
 //        while (iterator.hasNext()) {
 //            Log.d("BBB", iterator.next());
 //        }
-        observableNames.subscribe(new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                disposable = d;
-            }
-
-            @Override
-            public void onNext(String s) {
-                Log.d("BBB", s);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-                disposable.dispose();
-            }
-        });
+//        observableNames.subscribe(new Observer<String>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                disposable = d;
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//                Log.d("BBB", s);
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                disposable.dispose();
+//            }
+//        });
 
 
     }
